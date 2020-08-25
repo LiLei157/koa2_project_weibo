c73444da17731c20aa849fa1d767e22c67dbf990
LiLei157 <www.1213564228@qq.com>
2020-08-25 10:33:24 +0800

    mysql2模块install

---
+++ src/db/model/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-25 10:22:47
+ * @LastEditTime: 2020-08-25 10:23:12
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \koa2_project_weibo\src\db\model\index.js
+ */
+const User = require('./User')
+
+module.exports = {
+    User
+}
--- src/db/sync.js
+++ src/db/sync.js
@@ -1,13 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-08-25 09:27:19
- * @LastEditTime: 2020-08-25 09:38:36
+ * @LastEditTime: 2020-08-25 10:24:13
  * @LastEditors: Please set LastEditors
  * @Description: 数据库同步和测试
  * @FilePath: \koa2_project_weibo\src\db\sync.js
  */
 
 const seq = require('./seq')
+// 导入我们创建的数据模型，同步进数据库中
+require('./model/index')
 
 seq.authenticate().then(()=>{
     console.log('authen ok')
